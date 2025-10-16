5ab03cd57e9a4d9933c4b856ce9e69cc8ad07a5a
Juan Martinez <jtogni.developer@gmail.com>
2025-10-16 11:30:42 -0300

    fix: corregir la ruta de importación del archivo JSON en equipo.astro y ajustar la configuración de TypeScript en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-// @ts-check
+// @ts-ignore
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+/* import node from "@astrojs/node"; */
 import tailwindcss from "@tailwindcss/vite";
 
 import netlify from "@astrojs/netlify";
