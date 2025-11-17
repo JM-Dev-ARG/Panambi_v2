2505e6d7a34e50b75d0d15d8ddc69edd810c4b0c
JM-Dev <jtogni.developer@gmail.com>
2025-11-17 15:30:45 -0300

    cambios de seo y estructura de astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 /* import node from "@astrojs/node"; */
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://panambi.net',
+  integrations: [sitemap()],
 
   output: "server",
   adapter: netlify(),
